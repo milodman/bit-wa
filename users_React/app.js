692cef6b1a572ac0e39ec61fa6b211679ff07ed7
BIT Student <classroom@bgit.rs>
2018-03-16 16:21:34 +0100

    react-blogApp and react-users

--- users_React/app.js
+++ users_React/app.js
@@ -12,7 +12,23 @@ const Header = () => {
 
 const UserCard = props => {
     const { name, email, dob, img } = props;
-    return(
+
+    const date = dob => {
+        const date = new Date(dob);
+        const year = date.getFullYear();
+        const month = date.getMonth(); 
+        const day = date.getDate(); 
+         
+        return `${year}-${month}-${day}`
+    }
+
+    const emailHide = email => {
+        const monkey = email.indexOf("@");
+        const hidePart = email.substring(0,3) + "..." + email.substring(monkey - 2);
+        return hidePart;
+    }
+
+    return (
         <div>   
             <div className="col s12 m4">
                 <div className="card">
@@ -21,8 +37,8 @@ const UserCard = props => {
                         <span className="card-title">{name}</span>
                     </div>
                     <div className="card-content">
-                        <p>email:{email}<br/> 
-                        Date of birth:{dob}</p>
+                        <p>Email: {emailHide(email)}<br/> 
+                        Date of birth: {date(dob)}</p>
                     </div>
                 </div>
             </div>
